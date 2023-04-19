6d61343d576587a13c9f1afd1566c913cd38b985
Thananjayan <thananjayan1991@yahoo.com>
2023-04-19 14:09:36 +0530

    ccx

--- demo.py
+++ demo.py
@@ -34,11 +34,10 @@ def custom_header(logo_base64):
     </style>
     <div class="header">
         <div style="display:flex;align-items:center;">
-            <img src="data:image/png;base64,{logo_base64}" style="height:80px; max-height: 100%; object-fit: contain; margin-right:15px;"/>
+            <img src="data:image/png;base64,{logo_base64}" style="max-height:100%; max-width:120px; margin-right:15px;"/>
         </div>
-        <div>
-            <img src="flipick_coursebot.png" />
-        </div>
+
+        
         <div style="display:flex;align-items:center;margin-left:auto; top: 50%;">
             <a href="/" style="text-decoration:none; padding-top:40px;">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="100" viewBox="0 0 24 24" fill="none" stroke="#2953B3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home">
