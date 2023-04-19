6659cf08b3da74f8d78cc174b31bd9c5cbafff3a
Thananjayan <thananjayan1991@yahoo.com>
2023-04-19 15:50:39 +0530

    ffdd

--- app.py
+++ app.py
@@ -8,31 +8,75 @@ import xml.etree.ElementTree as ET
 from xml.dom import minidom
 from pathlib import Path
 from llama_index import download_loader
-from streamlit_option_menu import option_menu
+import base64
 
 st.set_page_config(page_title=None, page_icon=None,
                    layout="wide", initial_sidebar_state="collapsed")
 
+openai.api_key = os.getenv("API_KEY")
+
 hide_menu_style = """
         <style>
         #MainMenu {display:none;}
+        [data-testid="stHeader"]>header {{
+        display:none !important;
+        }}
+        .css-hqnn1b{display:none !important;}
         </style>
         """
 st.markdown(hide_menu_style, unsafe_allow_html=True)
 
-openai.api_key = os.getenv("API_KEY")
 
-choose = option_menu("App Gallery", ["About", "Photo Editing", "Project Planning", "Python e-Course", "Contact"],
-                     icons=['house', 'camera fill', 'kanban',
-                            'book', 'person lines fill'],
-                     menu_icon="app-indicator", default_index=0, orientation="horizontal",
-                     styles={
-    "container": {"padding": "5!important", "background-color": "#fafafa"},
-    "icon": {"color": "orange", "font-size": "25px"},
-    "nav-link": {"font-size": "16px", "text-align": "left", "margin": "0px", "--hover-color": "#eee"},
-    "nav-link-selected": {"background-color": "#02ab21"},
-})
+def get_image_base64(image_file):
+    with open(image_file, "rb") as img_file:
+        return base64.b64encode(img_file.read()).decode("utf-8")
 
+
+# Replace with the path to your logo image
+logo_image = "pages/flipick_coursebot.png"
+logo_base64 = get_image_base64(logo_image)
+
+# Define your header with the logo and home button
+
+
+def custom_header(logo_base64):
+    header = f'''
+    <style>
+        .header {{
+            height:130px;
+            background-color:white;
+            display:flex;
+            align-items:center;
+            padding:0px 20px;
+            position:fixed;
+            top:-40px;
+            left:0;
+            right:0;
+            z-index:1000;
+        }}
+    </style>
+    <div class="header">
+        <div style="display:flex;align-items:center;">
+            <img src="data:image/png;base64,{logo_base64}" style="margin-top:40px; margin-left:50px; max-height: 100%; object-fit: contain; margin-right:15px;"/>
+        </div>
+        <div style="display:flex;align-items:center;margin-left:auto; top: 50%;">
+            <a href="/" style="text-decoration:none; padding-top:40px;">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="100" viewBox="0 0 24 24" fill="none" stroke="#2953B3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home">
+                    <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+                    <polyline points="9 22 9 12 15 12 15 22"></polyline>
+                </svg>
+            </a>
+        </div>
+    </div>
+    '''
+    return header
+
+
+# Display the custom header in the Streamlit app
+st.markdown(custom_header(logo_base64), unsafe_allow_html=True)
+
+# Add an empty container to create space for the fixed header
+st.empty()
 
 st.title("CourseBot")
 st.caption("AI-powered course creation made easy")
