8bf618de635fcfc71e11442ada13f4e12ad1ab32
Thananjayan <thananjayan1991@yahoo.com>
2023-04-19 15:30:38 +0530

    cfv

--- pages/demo.py
+++ pages/demo.py
@@ -49,7 +49,7 @@ def custom_header(logo_base64):
             left:0;
             right:0;
             z-index:1000;
-            margin-top:-50px;
+            
         }}
     </style>
     <div class="header">
